e0392a6ae39887ea369b65d024db1709aff91a71
lowleetak <lowleetak@gmail.com>
2017-08-26 07:55:45 +0800

    Solution created

---
+++ XamarinStyleCop/XamarinStyleCop/XamarinStyleCopPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace XamarinStyleCop
+{
+    public partial class XamarinStyleCopPage : ContentPage
+    {
+        public XamarinStyleCopPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
